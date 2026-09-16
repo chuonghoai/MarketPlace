7fdb3772d162e95a8b6dc05359d111f1436c0010
Hoai Chuong <144915081+chuonghoai@users.noreply.github.com>
2026-09-16 14:37:47 +0700

    refactor base environtment for frontend web

--- web/src/core/config/env.config.ts
+++ web/src/core/config/env.config.ts
@@ -1,6 +1,5 @@
 export const ENV = {
     API_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
-    API_TIMEOUT: Number(
-        import.meta.env.VITE_API_TIMEOUT || 10000
-    ),
+    API_TIMEOUT: Number(import.meta.env.VITE_API_TIMEOUT) || 10000,
+    MODE: import.meta.env.MODE,
 };
